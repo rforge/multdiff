1bb9c74aecb045fe25e15383d93703b53af70621
nielsrhansen <nielsrhansen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-22 06:58:55 +0000

    ProcessData updated to version 0.6.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multdiff@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/processdata/R/ProcessData.R
+++ pkg/processdata/R/ProcessData.R
@@ -1,141 +1,72 @@
-setClass("ProcessData",
-         representation(
-                        markedPointProcess = "MarkedPointProcess",
-                        continuousProcess = "ContinuousProcess",
-                        pointPosition = "data.frame"
-           )
-         )
-
-setMethod("initialize","ProcessData",
-          function(.Object,
-                   markedPointProcess=new("MarkedPointProcess"),
-                   continuousProcess=new("ContinuousProcess")){
-            ## Compute a combined set of evaluation points and keep track of which
-            ## points are points from the marked point process.
-            
-            evalPosition <- data.frame(id = factor(c(as.character(getId(markedPointProcess)),
-                                         as.character(getId(continuousProcess))),
-                                         levels=levels(getId(markedPointProcess))),
-                                       position = c(getPosition(markedPointProcess),
-                                         getPosition(continuousProcess)),
-                                       pointer=c(seq(along=getId(markedPointProcess)),
-                                         rep(Inf,length(getId(continuousProcess)))))
-            
-            dup <- unlist(tapply(evalPosition$position,evalPosition$id,duplicated),use.names=FALSE)
-            evalPosition <- evalPosition[c(rep(TRUE,length(getId(markedPointProcess))),!dup[-c(seq(along=getId(markedPointProcess)))]),]
-
-            ord <- tapply(evalPosition$position,evalPosition$id,order)
-            ord <- unlist(lapply(levels(evalPosition$id),
-                                 function(id) which(evalPosition$id==id)[ord[[id]]]),use.names=FALSE)
-
-             where <- which(evalPosition$pointer[ord] < Inf)
-            .Object@pointPosition <- data.frame(pointer=evalPosition$pointer[ord][where],where=where)
-
-            j <- sapply(levels(evalPosition$id),
-                               function(i) {
-                                 findInterval(evalPosition[ord,"position"][evalPosition[ord,"id"]==i],
-                                              c(getPosition(continuousProcess)[getId(continuousProcess)==i],Inf),
-                                              all.inside=TRUE)},
-                        simplify=FALSE)
-
-            j <- unlist(lapply(levels(evalPosition$id),
-                        function(i) {
-                          which(getId(continuousProcess)==i)[j[[i]]]
-                        }),
-                        use.names=FALSE)
-           
-
-            value <- Matrix(as.matrix(getValue(continuousProcess))[j,,drop=FALSE])
-           .Object@continuousProcess <- new("ContinuousProcess",id=evalPosition[ord,"id"],position=evalPosition[ord,"position"],value=value)
-
-           
-            .Object@markedPointProcess <- markedPointProcess
-            
-            return(.Object)
+setMethod("dim", "ProcessData",
+          function(x) {
+            return(dim(x@unitData))
+          }
+          )
+                     
+setMethod("colNames", "ProcessData",
+          function(object, ...) {
+            return(names(object@unitData))
           }
           )
 
-setMethod("[",c("ProcessData","ANY","missing"),
-          function(x,i,j,drop=FALSE){
-            cp <- x@continuousProcess[i]
-            mpp <- x@markedPointProcess[x@pointPosition$pointer[x@pointPosition$where %in% i]]
-            new("ProcessData",
-                markedPointProcess=mpp,
-                continuousProcess=cp)
+setMethod("getUnitData", "ProcessData",
+          function(object, ...) {
+            return(object@unitData)
           }
           )
 
-            
-setMethod("getContinuousProcess","ProcessData",
-          function(object,pos){
-            return(object@continuousProcess)
+setMethod("[", "ProcessData",
+          function(x, i, j, ..., drop = FALSE) {
+            x@unitData <- x@unitData[i, j, drop = drop]
+            return(x)
           }
           )
 
-setMethod("getMarkedPointProcess","ProcessData",
-          function(object){
-            return(object@markedPointProcess)
+setMethod("[", c(x = "ProcessData", i = "ANY", j = "missing"),
+          function(x, i, j, ..., drop = FALSE) {
+            j <- seq_len(dim(as(x, "ProcessData"))[2])
+            x <- callGeneric(x, i, j, drop)
+            return(x)
           }
           )
 
-setMethod("getPointPosition","ProcessData",
-          function(object){
-            return(object@pointPosition)
+setMethod("[", c(x = "ProcessData", i = "missing", j = "ANY"),
+          function(x, i, j, ..., drop = FALSE) {
+            i <- seq_len(dim(as(x, "ProcessData"))[1])
+            x <- callGeneric(x, i, j, drop)
+            return(x)
           }
           )
 
-setMethod("getMarkTypePosition","ProcessData",
-          function(object,type){
-            return(object@pointPosition$where[object@markedPointProcess@markType[object@pointPosition$pointer]==type])
-          }
-          )           
-
-
-setMethod("plot",signature(x="ProcessData",y="missing"),
-          function(x,y,by=1,...){
-            if(dim(getValue(x@continuousProcess))[2]==0){
-              returnPlot <- plot(x@markedPointProcess) 
-            } else {
-              plotMarkedData <- getPlotData(x@markedPointProcess)
-              plotContinuousData <- getPlotData(x@continuousProcess,by=by)
-              
-              plotMarkedData$processType <- factor(rep("points",length.out=dim(plotMarkedData)[1]),
-                                                   levels=c("points","continuous"))
-              plotContinuousData$processType <- factor(rep("continuous",length.out=dim(plotContinuousData)[1]),
-                                                       levels=c("points","continuous"))
-
-             # plotData <- as.data.frame(table(rbind(plotMarkedData[,c("id","processType")],plotContinuousData[,c("id","processType")])))
-
-              low <- signif(min(plotContinuousData$value),1)
-              high <- signif(max(plotContinuousData$value),1)
-              prettyBreaks <- pretty(c(low,high))
-
-              numtype <- as.numeric(plotMarkedData$type)
-              breaks <- c(prettyBreaks,high + seq(1,max(numtype))*(high-low)/max(numtype))
-              numtype <- numtype*(high-low)/max(numtype)
-              plotMarkedData$numtype <- numtype + high
-             
-              returnPlot <-  ggplot() +
-                facet_grid(id ~ .,scales="free_y")  +
-                  geom_point(data=plotMarkedData,aes(x=position,y=numtype,colour=type)) +
-                    geom_line(data=plotContinuousData,aes(x=position,y=value,colour=variable)) +
-                      scale_y_continuous(breaks=breaks,name="",labels=c(as.character(prettyBreaks),levels(plotMarkedData$type))) +
-                        scale_colour_discrete("Variable",breaks=c(levels(plotMarkedData$type),levels(plotContinuousData$variable)))
+setMethod("subset", "ProcessData",
+          function(x, subset, select, ...) {
+            if (missing(subset)) 
+              r <- TRUE
+            else {
+              e <- substitute(subset)
+              r <- eval(e, x@unitData, parent.frame())
+              if (!is.logical(r)) 
+                stop("'subset' must evaluate to logical")
+              r <- r & !is.na(r)
             }
-            return(returnPlot)
+            if (missing(select)) 
+              vars <- TRUE
+            else {
+              nl <- as.list(seq(1L,dim(x@unitData)[2]))
+              names(nl) <- colnames(x@unitData)
+              vars <- eval(substitute(select), nl, parent.frame())
+            }
+            
+            return(x[r, vars])
           }
           )
 
-## Possible arguments to the following function are subset, mppOnly and cpOnly
-
-setMethod("subset","ProcessData",
-          function(x,...,mppOnly=FALSE,cpOnly=FALSE){
-            continuousProcess <- if(!mppOnly) subset(x@continuousProcess,...) else x@continuousProcess
-            markedPointProcess <- if(!cpOnly) subset(x@markedPointProcess,...) else x@markedPointProcess
-
-            return(new("ProcessData",markedPointProcess,continuousProcess))
-          }
-          )
-            
-            
           
+setReplaceMethod("setUnitData", c("ProcessData", "data.frame"),
+          function(object, value) {
+            object@unitData <- value
+            validObject(object)
+            return(object)
+          }
+          )
